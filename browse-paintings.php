95677126b7488103277dc1727a52685f5eedd359
PedroJanikian <43455987+PedroJanikian@users.noreply.github.com>
2020-12-04 19:16:19 -0700

    Create header and browse (filter) page. Very barebones

---
+++ browse-paintings.php
@@ -0,0 +1,31 @@
+<?php
+require_once 'header.php';
+
+
+?>
+
+<!DOCTYPE html>
+<html lang=en>
+<head>
+    <title>Lab 14</title>
+    <meta charset=utf-8>
+    <link href='http://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" rel="stylesheet">
+</head>
+<body>
+<?= createHeader(); ?>
+<main>
+
+
+
+    <section></section>
+    <section></section>
+
+
+
+</main>
+<footer class="ui black inverted segment">
+  <div class="ui container">This is the footer</div>
+</footer>
+</body>
---
+++ header.php
@@ -0,0 +1,13 @@
+<?php
+function createHeader() {
+    echo 
+        "<article>
+            <header>
+                <h1>Heading</h1>
+                <p>by Pedro</p>
+                <p>some more info</p>
+            </header>
+            <p>Lorem Impsum</p>
+        </article>";
+}
+?>
