92923ddeb0cfb5000573ea7321ccdd2905a2e0f5
PedroJanikian <43455987+PedroJanikian@users.noreply.github.com>
2020-12-09 13:43:06 -0700

    Changed css style position

---
+++ about.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang=en>
+<head>
+    <title>Lab 14</title>
+    <meta charset=utf-8>
+    <!-- These 3 references are taken from Lab14a. Might remove and remodel to our own CSS if we have time. -->
+    <link href='http://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" rel="stylesheet">
+</head>
+<body>
+<main>
+    <?php include("header.php");?>
+</main>
+</body>
