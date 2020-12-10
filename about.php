ed5258f1e76dfbaeb5c2a1b23c1c54bd51adb4c3
PedroJanikian <43455987+PedroJanikian@users.noreply.github.com>
2020-12-10 04:41:52 -0700

    Quick fix to ABout page

--- about.php
+++ about.php
@@ -8,18 +8,9 @@
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" rel="stylesheet">
 </head>
-<body>
-<style>
-       body{
-background-image: url('images/payson-wick-vGLXKqCY66Y-unsplash.jpg');
+<body style="background-image: url(./images/payson-wick-vGLXKqCY66Y-unsplash.jpg);
 background-size: cover;
-background-repeat: no-repeat;
-margin: 50px auto;
-    text-align: center;
-    width: 100%;
-}
-
-</style>
+background-repeat: no-repeat;">
 <?php include("header.php");?>
 <main>
     
